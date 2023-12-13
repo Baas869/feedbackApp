89738bb992bb4cbddddccb176b71bd7a4c3a8188
Baas869 <sulaimanboladale869@gmail.com>
2023-12-13 09:06:30 -0800

    new changes

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,31 +3,31 @@ import { useContext } from 'react'
 // import PropTypes from 'prop-types'
 import Feedbackitem from "./Feedbackitem"
 import FeedbackContext from '../context/FeedbackContext'
+import Spiner from './shared/Spiner'
 
 
 function FeedbackList() {
-  const {feedback} = useContext(FeedbackContext)
-    if(!feedback || feedback.length === 0) {
+  const { feedback, isLoading } = useContext(FeedbackContext)
+    if(!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No feedback yet</p>
     }
-
-    return (
+    return isLoading ? <Spiner /> : (
       <div className='feedback-list'>
         <AnimatePresence>
-            {feedback.map((item) => (
-              <motion.div key={item.id}
-              initial={{opacity: 0}}
-              animate={{opacity: 1}}
-              exit={{opacity: 0}}
-              >
-                <Feedbackitem key={item.id} item={item} />
-              </motion.div>
-            ))}
-        </AnimatePresence>
-           
-        
-      </div>
-    )
+          {feedback.map((item) => (
+            <motion.div key={item.id}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            >
+              <Feedbackitem key={item.id} item={item} />
+          </motion.div>
+        ))}
+    </AnimatePresence>
+       
+    
+  </div>)
+   
 
     // return (
   //   <div className='feedback-list'>
---
+++ src/components/shared/Spiner.jsx
@@ -0,0 +1,8 @@
+import spinner from '../assets/spinner.gif'
+function Spiner() {
+  return <img src={spinner} alt='Loading...' style={{
+    width: '100px', margin: 'auto', display: 'block'
+  }} />
+}
+
+export default Spiner
